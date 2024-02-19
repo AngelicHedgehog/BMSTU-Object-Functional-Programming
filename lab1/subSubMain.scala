82bf73c861d8e59df240e17ed35a2b79808a2a32
Sergey Dmitrievich <serj_1313@mail.ru>
2024-02-19 19:19:10 +0300

    lab1: выполнил более полное условие стороннего варианта лабы

--- lab1/subSubMain.scala
+++ lab1/subSubMain.scala
@@ -9,6 +9,37 @@ val maxSumKadan: List[Int] => Int = {
         }
 }
 
+val maxSeqBySum: List[Int] => (Int, Int) = {
+    def maxSeqBySumWithSum: List[Int] => (Int, Int, Int) = {
+        case Nil                    => (0, 0, 0)
+        case (x :: xs) if (x < 0)   =>
+            maxSeqBySumWithSum(xs) match {
+                case (0, 0, 0)      => (0, 0, 0)
+                case (a, b, sum)    => (a + 1, b + 1, sum)
+            }
+        case (x :: Nil)             => (0, 1, x)
+        case (x :: y :: tail)       =>
+            maxSeqBySumWithSum((x + y) :: tail) match {
+                case (0, b, sum) if (sum > x)   => (0, b + 1, sum)
+                case (0, _, _)                => (0, 1, x)
+                case (a, b, sum) if (sum > x)   => (a + 1, b + 1, sum)
+                case (_, _, _)                  => (0, 1, x)
+            }
+    }
+    xs => maxSeqBySumWithSum(xs) match {
+        case (a, b, sum) => (a, b)
+    }
+}
+
 // https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%2026/2585.htm
-println(maxSumKadan(List(-3, 4, 9, -2, -5, 8, -3))) // 14
-println(maxSumKadan(List(5, -3, 1, -7, 4, -2, 4, -1, -8, 2))) // 6
+val test1 = List(-3, 4, 9, -2, -5, 8, -3)
+println(maxSumKadan(test1)) // 14
+println(maxSeqBySum(test1)) // 1 6
+
+val test2 = List(5, -3, 1, -7, 4, -2, 4, -1, -8, 2)
+println(maxSumKadan(test2)) // 6
+println(maxSeqBySum(test2)) // 4 7
+
+val test3 = List(-1, 10, -1, -1, -1, 10, -1, -1, -1, 10)
+println(maxSumKadan(test3)) // 24
+println(maxSeqBySum(test3)) // 1 10
