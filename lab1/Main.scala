37664b8692800a52a6a7bb20bb01ff05bb7d6c40
Sergey Dmitrievich <serj_1313@mail.ru>
2024-02-19 11:37:50 +0300

    lab1: сделал дома вариант 28 (надеюсь попал)

--- lab1/Main.scala
+++ lab1/Main.scala
@@ -1,3 +1,14 @@
-object Main extends App {
-    println("Hello, world")
+val sublists: (List[Int], Int) ⇒ List[List[Int]] = {
+    case (Nil, p) if (0 <= p)       ⇒ List(Nil)
+    case (Nil, _)                   ⇒ Nil
+    case (x :: xs, p) if (x <= p)   ⇒ List(x) :: sublists(xs, p)
+    case (x :: Nil, p)              ⇒ sublists(Nil, p)
+    case (x :: y :: xs, p)          ⇒
+        sublists((x + y) :: xs, p) match {
+            case (Nil) | (Nil :: Nil)   ⇒ Nil
+            case (Nil :: _)             ⇒ Nil // невалидный случай
+            case ((aa :: aas) :: as)    ⇒ (x :: (aa - x) :: aas) :: as
+        }
 }
+
+println(sublists(List(0, 0, 1, -2, -5, 10, -3, 0, 1, -10, 1, -2), 0))
